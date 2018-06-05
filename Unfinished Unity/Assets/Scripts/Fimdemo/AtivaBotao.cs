49e57edf40326d63293aeb335318e26d4801abfc
Spositogui <developersposito@gmail.com>
2018-06-05 15:09:07 -0300

    EndDemoScene

---
+++ Unfinished Unity/Assets/Scripts/Fimdemo/AtivaBotao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtivaBotao : MonoBehaviour
+{
+    public GameObject botaoMenu;
+    public void AtivarBotaoMenu()
+    {
+        botaoMenu.gameObject.SetActive(true);
+    }
+}
---
+++ Unfinished Unity/Assets/Scripts/Fimdemo/BackToMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour {
+
+
+
+    public void DemoEnd_ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
